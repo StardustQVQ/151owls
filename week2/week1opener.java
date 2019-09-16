42d68fd920a1be9b241a827bf06545b923262b20
Robert Klock <rklock@oberlin.edu>
2019-09-16 11:32:59 -0400

    relevant resources for week2

---
+++ week2/week1opener.java
@@ -0,0 +1,15 @@
+public class week1opener{
+    public static void main (String[] args){
+	int number = 4;
+	double alpha = -1.0;
+	if (number>0){
+	    if(alpha>0){
+		System.out.println("Here I am!");
+	    }
+	    else{
+		System.out.println("No, I'm here!");
+	    }
+	}
+	System.out.println("No, actually, I'm here!");
+    }
+}
